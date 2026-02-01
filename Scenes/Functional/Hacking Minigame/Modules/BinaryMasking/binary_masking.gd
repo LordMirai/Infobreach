ccac5c08ce63f3ebd60a184740c7f3fd8aebab15
Lord Mirai (未来) <pilotcraft1001@yahoo.com>
2026-02-01 02:28:43 +0200

    First working binary masking

--- Scenes/Functional/Hacking Minigame/Modules/BinaryMasking/binary_masking.gd
+++ Scenes/Functional/Hacking Minigame/Modules/BinaryMasking/binary_masking.gd
@@ -6,6 +6,8 @@ var output: int = 2000 # whatever it is, do not go past 4095
 var method: String = "Addition" # or "XOR"
 var mask: String = ""
 
+var see_as_binary: bool = false
+
 var symbol_table = {
 	"Addition": "+",
 	"XOR": "⊕"
@@ -20,6 +22,12 @@ func sub_init():
 	input = randi_range(0, 4096)
 	output = randi_range(100, 4096)
 	method = symbol_table.keys()[randi() % symbol_table.size()]
+
+	if method == "Addition" and output <= input:
+		var temp = input
+		input = output
+		output = temp
+
 	_update_labels()
 
 func _get_mask() -> Array:
@@ -35,14 +43,33 @@ func _get_mask() -> Array:
 	return [mask_str, mask_value]
 
 func _get_temp():
-	pass
+	if method == "Addition":
+		return input + _get_mask()[1]
+	elif method == "XOR":
+		return input ^ _get_mask()[1]
 
 func _update_labels():
 	print("update label called?")
 	var msk = _get_mask()
 	print("Mask: ", msk[0], " Value: ", msk[1])
-	$Container/Background/Input.text = "Input: " + str(input)
+
+	var _tmp = _get_temp()
+
+	# print("Button index %d pressed, state %s, value %d" % [index, str(toggled_on), value])
+
+	var _inp = String.num_int64(input, 2) if see_as_binary else input
+	var _outp = String.num_int64(output, 2) if see_as_binary else output
+	var _temp = String.num_int64(_tmp, 2) if see_as_binary else _tmp
+
+	$Container/Background/Input.text = "Input: " + str(_inp)
 	$Container/Background/Symbol.text = symbol_table[method]
-	$Container/Background/Output.text = "Output: " + str(output)
+	$Container/Background/Output.text = "Output: " + str(_outp)
 	$Container/Background/Mask.text = "Mask: " + msk[0] + "  " + str(msk[1])
-	$Container/Background/Out_Temp.text = "Temp: " + str(_get_temp())
+	$Container/Background/Out_Temp.text = "Temp: " + str(_temp)
+
+	if _tmp == output:
+		complete_module()
+
+func _on_binary_view_btn_pressed() -> void:
+	see_as_binary = !see_as_binary
+	_update_labels()
