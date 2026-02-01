6be187ab31628a410c3f7af98a7812823872c032
OpGeorge <opgeorge.dev@gmail.com>
2026-02-01 03:05:14 +0200

    feat: add camera fov

--- Scenes/Functional/CameraSystem/Scripts/main_camera2.gd
+++ Scenes/Functional/CameraSystem/Scripts/main_camera2.gd
@@ -10,15 +10,28 @@ var isCameraDisabled : int = 0
 var beforeHackFrame: int =0
 var beforeHackLoopback: int = -1
 
+@onready var cameraFOVPolygon : CollisionPolygon2D = $Area2D/CollisionPolygon2D
+var currentColor = Color()
+var colorFOV = Color(0,1,0.5,0.5)
+var colorWarn = Color(0,1,1,0.7)
+var colorDetect = Color(1,0,0,0.7)
+
 func _ready() -> void:
 	$CameraSprites.set_frame(0)
 	$CameraSprites.set_speed_scale(0.0)
 	$Timers/Timer.start(3)
 	$WarningSprites.set_frame(0)
+	currentColor = colorFOV
 	
+func _draw() -> void:
+	
+	if cameraFOVPolygon:
+		draw_set_transform($Area2D.position,$Area2D.rotation, cameraFOVPolygon.scale)
+		draw_polygon(cameraFOVPolygon.polygon,[currentColor])
+	draw_set_transform(Vector2.ZERO,0.0,Vector2.ONE)
 	
 func _process(delta: float) -> void:
-	pass
+	queue_redraw()
 	
 	
 var loopback = -1
@@ -42,11 +55,13 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		$WarningSprites.set_frame(1)
 		$Timers/WarnTimer.start(TIME_OF_WARNING)
 		$Timers/DangerTimer.start(TIME_OF_WARNING+TIME_OF_DETECTION)
+		currentColor = colorWarn
 		print("WARNING YOU ARE BEING DETECTED")
 
 func _on_warn_timer_timeout() -> void:
 	$Timers/WarnTimer.stop()
 	print("LAST WARNING THE MAINFRAME IS CLOSE TO IDENTIFYING YOU")
+	currentColor = colorDetect
 	$WarningSprites.set_frame(2)
 	pass # Replace with function body.
 	
@@ -62,6 +77,7 @@ func _on_area_2d_area_exited(area: Area2D) -> void:
 		$WarningSprites.set_frame(0)
 		$Timers/DangerTimer.stop()
 		$Timers/WarnTimer.stop()
+		currentColor = colorFOV
 	# Replace with function body.
 	
 	
@@ -74,4 +90,6 @@ func _on_disabled_timer_timeout() -> void:
 	$Timers/DisabledTimer.stop()
 	$DeviceInteractable.hacked = false
 	$CameraSprites.modulate.a = 1.0
+	currentColor.a = 1.0
+	currentColor = colorFOV
 	
